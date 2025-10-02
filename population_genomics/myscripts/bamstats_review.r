ce39b7694756c2fece2819d30d8b2e32cdcd2acc
Max Russell Mcclure <mrmcclur@node413.cluster>
2025-10-02 17:05:56 -0400

    quick change to bamstats

--- population_genomics/myscripts/bamstats_review.r
+++ population_genomics/myscripts/bamstats_review.r
@@ -1,7 +1,7 @@
 # Reviewing Bam Stats
 
 getwd()
-#setwd("~/population_genomics/myresults/")
+setwd("~/projects/eco_genomics_2025/population_genomics/myresults")
 
 stats <- read.table("2032.stats.txt", header = TRUE, sep = "")
 
